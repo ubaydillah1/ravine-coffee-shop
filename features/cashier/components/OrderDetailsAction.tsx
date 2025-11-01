5ca32d7515cec1e788fe22cab868d469d81f4d66
ubaydillah1 <ubaydillah1737@gmail.com>
2025-11-02 03:57:39 +0700

    fix: mismatch props lint error

--- features/cashier/components/OrderDetailsAction.tsx
+++ features/cashier/components/OrderDetailsAction.tsx
@@ -1,23 +1,22 @@
 "use client";
 
-import React, { useState } from "react";
 import Eye from "@/public/assets/icons/eye.svg";
-import OrderDetailsOverlay from "./OrderDetailsOverlay";
+// import OrderDetailsOverlay from "./OrderDetailsOverlay";
 
 const OrderDetailsAction = () => {
-  const [openModal, setOpenModal] = useState(false);
+  // const [openModal, setOpenModal] = useState(false);
 
-  const closeModal = () => setOpenModal(false);
+  // const closeModal = () => setOpenModal(false);
   return (
     <>
       <button
         className="size-[36px] rounded-full border border-neutral-n300 flex-center cursor-pointer hover:bg-neutral-n100"
-        onClick={() => setOpenModal(true)}
+        // onClick={() => setOpenModal(true)}
       >
         <Eye className="text-neutral-n700 size-[16px]" />
       </button>
 
-      <OrderDetailsOverlay closeModal={closeModal} openModal={openModal} />
+      {/* <OrderDetailsOverlay closeModal={closeModal} openModal={openModal} /> */}
     </>
   );
 };
